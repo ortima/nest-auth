0d5fc27c451a8e196dc446c5eae7d8c98b04a23e
ortima <account.net@mail.ru>
2024-10-06 23:57:44 +0300

    🚧 (feat): Added auth entity

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+
+import { RegisterDto } from '@/auth/dto/register.dto';
 
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-	constructor(private readonly authService: AuthService) {}
+	public constructor(private readonly authService: AuthService) {}
+
+	@Post('register')
+	@HttpCode(HttpStatus.OK)
+	public async register(@Body() dto: RegisterDto) {
+		return this.authService.register(dto);
+	}
 }
