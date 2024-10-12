8fc954de03d0ddeb1b47d8f69a2bf7a22147dc5b
ortima <account.net@mail.ru>
2024-10-12 19:51:07 +0300

    🛂 (feat): Added controller to protected route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+
+import { Authorization } from '@/auth/decorators/auth.decorator';
+import { Authorized } from '@/auth/decorators/authorized.decorator';
 
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
+
+	@Authorization()
+	@HttpCode(HttpStatus.OK)
+	@Get('profile')
+	public async findProfile(@Authorized('id') userId: string) {
+		return this.userService.findById(userId);
+	}
 }
